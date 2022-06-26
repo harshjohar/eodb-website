a5d38774b5eb16aefa42219d36f1817af762c4ec
Harsh Johar <hpsj3112@gmail.com>
2022-06-26 22:59:46 +0530

    getting started

--- src/common/PageLayout.js
+++ src/common/PageLayout.js
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar"
 
 function PageLayout({children}) {
   return (
-    <div className="h-screen w-screen overflow-hidden flex flex-col">
+    <div className="h-screen w-screen overflow-hidden flex flex-col font-poppins">
         <Navbar />
         {children}
     </div>
--- src/index.js
+++ src/index.js
@@ -7,20 +7,26 @@ import AuthScreen from "./screens/AuthScreen";
 import Error from "./screens/Error";
 import Login from "./components/Login";
 import Register from "./components/SignUp";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        <Provider store={store}>
+
         <BrowserRouter>
             <Routes>
                 <Route index element={<HomeScreen />} />
                 <Route path="/" element={<HomeScreen />} />
-                <Route path="auth" element={<AuthScreen />}>
+                <Route path="auth/*" element={<AuthScreen />}>
                     <Route path="login" element={<Login />} />
                     <Route path="register" element={<Register />} />
+                    <Route path="*" element={<Error />} />
                 </Route>
                 <Route path="*" element={<Error />} />
             </Routes>
         </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
